bebf56a3927b36ba2e9af59b49a0946f5114eab5
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2021-05-07 14:50:18 +0100

    enable custom_progress

--- lib/customized.dart
+++ lib/customized.dart
@@ -6,5 +6,6 @@ export 'src/custom_check.dart';
 export 'src/custom_switch.dart';
 export 'src/custom_text.dart';
 export 'src/custom_button.dart';
+export 'src/custom_progress.dart';
 
 SizedBox get empty => SizedBox();
